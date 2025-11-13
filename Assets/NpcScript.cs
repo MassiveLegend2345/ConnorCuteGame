91e345e0ce6fe9992b646994ecc94621f084b2e5
MassiveLegend2345 <cdunne077@gmail.com>
2025-11-13 17:44:40 +0000

    fa
    fafafaf

---
+++ Assets/NpcScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+/*
+
+
+    //[RequiredComponent(typeof(NavMeshAgent))]
+    //[RequiredComponent(typeof(NavMeshAgent))]
+    public class NpcScript : MonoBehaviour
+    {
+       // public NavmeshAgent Agent;
+
+      //  public Animator Animator; 
+
+        public float CurrentSpeed
+        {
+            get { return Agent.velocity.magnitude; }
+        }
+        private void Awake()
+        {
+            Agent = GetComponent<NavMeshAgent>();
+           // Animator = GetComponent<Animator>(); 
+
+        }
+    }
+*/
